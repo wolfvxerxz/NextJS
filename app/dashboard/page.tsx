55aa1da898e27fac7f826426e4223c802c373334
Wolf <wolfvxerxz@gmail.com>
2024-06-29 11:00:25 +0200

    xd

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,19 @@
-export default function Page() {
-    return <p>Dashboard Page</p>;
+import { sql } from "@vercel/postgres";
+
+export default async function Cart({
+    params
+}: {
+    params: { user: string }
+}): Promise<JSX.Element> {
+    const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
+
+    return (
+        <div>
+            {rows.map((row) => (
+                <div key={row.id}>
+                    {row.id} - {row.quantity}
+                </div>
+            ))}
+        </div>
+    );
 }
